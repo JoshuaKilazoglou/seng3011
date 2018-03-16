debe6912f1a0f2310bc374dce70b742b97343af0
Unknown <joshua.kilazoglou@gmail.com>
2018-03-17 10:14:41 +1100

    Initial commit

---
+++ 3011.py
@@ -0,0 +1,18 @@
+from flask import Flask
+import requests
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('/getPage', methods=['GET'])
+def get_page():
+    return requests.get("http://graph.facebook.com/v2.11/820882001277849?access_token=2019883274951221|1c9281343bdcde168cdad00e354fd2aa")
+
+
+if __name__ == '__main__':
+    app.run()
