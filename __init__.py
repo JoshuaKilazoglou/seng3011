773493f921202786dc9fcb56c4cb3346fe7b7ba4
Unknown <joshua.kilazoglou@gmail.com>
2018-03-26 15:50:35 +1100

    Added a bit more to test call

--- __init__.py
+++ __init__.py
@@ -1,19 +1,25 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 import requests
 
 app = Flask(__name__)
 
+access_token = '2019883274951221|1c9281343bdcde168cdad00e354fd2aa'
 
 @app.route('/')
 def hello_world():
     return render_template("index.html")
 
-
-@app.route('/test', methods=['GET'])
+# example: http://localhost:5000/api/v0.1/page?company=woolworths&fields=id,name,fan_count,website
+@app.route('/api/v0.1/page', methods=['GET'])
 def get_page():
-    return requests.get("http://graph.facebook.com/v2.11/820882001277849?access_token=2019883274951221|1c9281343bdcde168cdad00e354fd2aa")
-
+    company = request.args.get('company')
+    fields = request.args.get('fields')
+    params = {'fields' : fields, 'access_token' : access_token}
+    print(params)
+    response = requests.get(f'https://graph.facebook.com/v2.11/{company}', params)
+    return (response.text, response.status_code, response.headers.items())
 
 if __name__ == '__main__':
     app.run()
